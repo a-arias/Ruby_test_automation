ad64ca5b27284500c2e8cfeffc29daa8de88016a
aarias <abraham.arias@moduscreate.com>
2017-10-17 06:30:12 -0700

    first commit on development

--- simple_test/test_google.rb
+++ simple_test/test_google.rb
@@ -3,12 +3,3 @@ arr1 = Array.new 3
 arr1[0]="SF"
 arr1[1]="LA"
 arr1[2]="NY"
-
-puts "From puts: #{arr1}"
-puts "From puts: #{arr2}"
-puts "From puts: #{arr3}"
-puts "From puts: #{arr1[1]}"
-
-puts "From puts: #{arr1[2]}"
-puts "From puts: #{arr1[3]}"
-puts "From puts: #{arr1[4]}"
